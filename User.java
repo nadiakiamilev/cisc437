11e672dbe57d47d5743c7190ad31230e0dcd73df
nadiakiamilev <nadia.kiamilev@gmail.com>
2014-12-05 17:18:41 -0500

    queries are working!!!

--- User.java
+++ User.java
@@ -19,73 +19,95 @@ public class User {
 	}
 
 	// Method to execute administration operation
-	public void adminExecuteOperation(int option, String dbname, String userID, String password) {
-		System.out.println("We are going to execute this operation: " );
+	public void adminExecuteOperation(String dbname, String userID, String password) {
 
-		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+dbname, userID, password);
+		int adminChoice=0;
+		boolean exit = false;
 
-			switch(option) {
-				case 1: //update
-					System.out.println("Please Enter the Story Title to Update: ");
-					BufferedReader storyOld = new BufferedReader(new InputStreamReader(System.in));
-					String storyOldTitle = storyOld.readLine();
-					System.out.println(storyOldTitle);
+		String adminOptionsText = " \n ADMIN MENU: \n " + "1. Update a story title \n " 
+			+ "2. Delete a specific instance \n "
+			+ "3. Quit program \n ";
 
-					System.out.println("Please Enter the New Story Title: ");
-					BufferedReader storyNew = new BufferedReader(new InputStreamReader(System.in));
-					String storyNewTitle = storyOld.readLine();
-					System.out.println(storyNewTitle);
-					
-					String sqlStatement = "UPDATE story " 
-						+ "SET title=" + "'" + storyNewTitle + "'"
-						+ " WHERE " + "title=" + "'"+ storyOldTitle + "'";
-					
-					System.out.println(sqlStatement);
-					try {
-						Statement st1 = cn.createStatement();
-						st1.executeUpdate(sqlStatement); 
-						System.out.println("We updated " + storyOldTitle + " with the new story title " + storyNewTitle);
-					}
-					catch (SQLException e) {
-						System.out.println("Query failed: " + e);
-					}
+		while(!exit){			
+			System.out.println("You're in admin mode. You can now do the following: ");
+			System.out.println(adminOptionsText);
+			BufferedReader inAdminChoice = new BufferedReader(new InputStreamReader(System.in));
 
-					break;
-				case 2: //delete
-					System.out.println("Please Enter the First Name of the Person You Want to Delete: ");
-					BufferedReader firstName = new BufferedReader(new InputStreamReader(System.in));
-					String first_Name = firstName.readLine();
-					System.out.println(first_Name);
-
-					System.out.println("Please Enter the Last Name of the Person You Want to Delete: ");
-					BufferedReader lastName = new BufferedReader(new InputStreamReader(System.in));
-					String last_Name = lastName.readLine();
-					System.out.println(last_Name);
-
-					String sqlDelStatement = "DELETE FROM person "
-						+ "WHERE " + "first_name=" + "'" + first_Name + "'"
-						+ " last_name=" + "'" + last_Name + "';";
-
-					try {
-						Statement st2 = cn.createStatement();
-						st2.executeUpdate(sqlDelStatement);
-						System.out.println("You have successfully deleted " + first_Name + " " + last_Name + " from the database.");
-					}
-					catch (SQLException e) {
-						System.out.println("That person does not exist. Query failed: " + e );
-					}
-					break;
-				case 3: 
-					System.out.println("You opted to return to the main menu.");
-					break;
+			try {
+				adminChoice = Integer.parseInt(inAdminChoice.readLine()); //admin update/delete choice
+				System.out.println("Great choice! " + adminChoice);
+			} 
+			catch (IOException e){
+				System.out.println("Failure to get admin choice");
 			}
 
-		}
+			try {
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+				cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+dbname, userID, password);
+
+				switch(adminChoice) {
+					case 1: //update
+						System.out.println("Please Enter the Story Title to Update: ");
+						BufferedReader storyOld = new BufferedReader(new InputStreamReader(System.in));
+						String storyOldTitle = storyOld.readLine();
+
+						System.out.println("Please Enter the New Story Title: ");
+						BufferedReader storyNew = new BufferedReader(new InputStreamReader(System.in));
+						String storyNewTitle = storyOld.readLine();
+
+						String sqlStatement = "UPDATE story " 
+							+ "SET title=" + "'" + storyNewTitle + "'"
+							+ " WHERE " + "title=" + "'"+ storyOldTitle + "'";
+						
+						System.out.println(sqlStatement);
+						try {
+							Statement st1 = cn.createStatement();
+							st1.executeUpdate(sqlStatement); 
+							System.out.println("We updated " + storyOldTitle + " with the new story title " + storyNewTitle);
+						}
+						catch (SQLException e) {
+							System.out.println("Query failed: " + e);
+						}
+
+						break;
+					case 2: //delete
+						System.out.println("Please Enter the First Name of the Person You Want to Delete: ");
+						BufferedReader firstName = new BufferedReader(new InputStreamReader(System.in));
+						String first_Name = firstName.readLine();
+
+						System.out.println("Please Enter the Last Name of the Person You Want to Delete: ");
+						BufferedReader lastName = new BufferedReader(new InputStreamReader(System.in));
+						String last_Name = lastName.readLine();
+
+						String sqlDelStatement = "DELETE FROM person "
+							+ "WHERE " + "first_name=" + "'" + first_Name + "'"
+							+ " last_name=" + "'" + last_Name + "';";
+
+						try {
+							Statement st2 = cn.createStatement();
+							st2.executeUpdate(sqlDelStatement);
+							System.out.println("You have successfully deleted " + first_Name + " " + last_Name + " from the database.");
+						}
+						catch (SQLException e) {
+							System.out.println("That person does not exist. Query failed: " + e );
+						}
+						break;
+					case 3: 
+						System.out.println("Quitting Program.");
+						exit = true;
+						break;
+					default:
+						System.out.println("Incorrect Option Selected, sorry!");
+				}
+			}
 		catch (Exception e) {
 			System.out.println("connection failed: " + e);
 		}
+
+		}
+
+		
+
 	}
 
 	/*******BHAVIN YOUR CODE GOES HERE!!***********/
@@ -93,14 +115,14 @@ public class User {
 		int userChoice=0;
 		boolean exit = false;
 
-		String userOptionsText = "1. Find all people in the database \n"
+		String userOptionsText = " \n USER MENU: \n"
+		+ "1. Find all people in the database \n"
 		+ "2. Find stories from requested person \n"
 		+ "3. Find stories from a specific country \n"
 		+ "4. Find stories from a range of years \n"
 		+ "5. Quit the program \n ";
 
 		while(!exit){
-			System.out.println("hey we are in user status");
 			System.out.println(userOptionsText);
 			System.out.print("select your choice and press enter: ");
 			BufferedReader inUserChoice = new BufferedReader(new InputStreamReader(System.in));
@@ -210,7 +232,7 @@ public class User {
 
 						break;
 					case 5: 
-						System.out.println("option 5 selected");
+						System.out.println("Quitting Program.");
 						exit = true;
 						break;
 
@@ -255,21 +277,12 @@ public class User {
 			int typeUser = Integer.parseInt(str);
 
 			if (typeUser == 1) {
-				System.out.println("You're in admin mode. You can now do the following: ");
-				System.out.println(adminOptionsText);
-				BufferedReader inAdminChoice = new BufferedReader(new InputStreamReader(System.in));
-				int adminChoice = Integer.parseInt(inAdminChoice.readLine()); //admin update/delete choice
-				System.out.println("Great choice! " + adminChoice);
+				
 				User userTmp = new User(dbname, userID, password);
-				userTmp.adminExecuteOperation(adminChoice, dbname, userID, password);
+				userTmp.adminExecuteOperation(dbname, userID, password);
 			}
 
 			else if (typeUser == 2 ) {
-				// System.out.println("hey we are in user status");
-				// System.out.println(userOptionsText);
-				// BufferedReader inUserChoice = new BufferedReader(new InputStreamReader(System.in));
-				// int userChoice = Integer.parseInt(inUserChoice.readLine()); //user's query choice
-				// System.out.println("Great choice! " + userChoice);
 				User userTmp = new User(dbname, userID, password);
 				userTmp.userExecuteOperation(dbname, userID, password);
 			}
