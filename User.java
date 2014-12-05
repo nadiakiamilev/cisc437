7262d8f8de3e4b7b48a80d9bf0cf9c01a58b6f94
nadiakiamilev <nadia.kiamilev@gmail.com>
2014-12-05 00:06:47 -0500

    added framework for execute methods

--- User.java
+++ User.java
@@ -17,12 +17,11 @@ public class User {
 		currentResults = null;
 		currentItem = null;
 
-		try
-		{
+		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 			cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+dbname, userID, password);
-		}catch (Exception e)
-		{
+		}
+		catch (Exception e) {
 			System.out.println("connection failed: " + e);
 		}
 
@@ -36,95 +35,9 @@ public class User {
 				System.out.println("Database: "+rs1.getString(1));
 			}
 			st1.close();
-		}catch (SQLException e) {
+		}
+		catch (SQLException e) {
 			System.out.println("Query failed: " + e);
-		}
-
-		try
-		{
-			System.out.println("use " + dbname);
-			Statement st2 = cn.createStatement();
-			st2.executeUpdate("use " + dbname);
-		}catch (SQLException e) {
-			System.out.println("Update failed: " + e);
-		}
-
-		try
-		{
-			System.out.println("show tables");
-			Statement st4 = cn.createStatement();
-			ResultSet rs4 = st4.executeQuery("show tables");
-			while (rs4.next())
-			{
-				System.out.println("Table: "+rs4.getString(1));
-			}
-			st4.close();
-		}catch (SQLException e) {
-			System.out.println("Query failed: " + e);
-		}
-
-		try
-		{
-			System.out.println("Show People in DB");
-			Statement st5 = cn.createStatement();
-			ResultSet rs5 = st5.executeQuery("SELECT * FROM person");
-			while (rs5.next())
-			{
-				System.out.print(rs5.getString(1)+", ");
-				System.out.print(rs5.getString(2)+", ");
-				System.out.print(rs5.getString(3)+", ");
-				System.out.println(rs5.getString(4));
-			}
-		}catch (SQLException e) {
-			System.out.println("Query failed: " + e);
-		}
-		try
-		{
-			System.out.println("Bhavin's Story");
-			Statement st6 = cn.createStatement();
-			ResultSet rs6 = st6.executeQuery("SELECT description FROM (story NATURAL JOIN person_story NATURAL JOIN person) WHERE first_name='Bhavin'");
-			while (rs6.next())
-			{
-				System.out.println(rs6.getString(1));
-			}
-		}catch (SQLException e){
-			System.out.println("Query failed: " + e);
-		}
-		try
-		{
-			System.out.print("Input a name:");
-			BufferedReader in1 = new BufferedReader(new InputStreamReader(System.in));
-			try {
-				str = "'"+in1.readLine()+"'";
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			String input = "SELECT description FROM (story NATURAL JOIN person_story NATURAL JOIN person) WHERE first_name=" +str;
-			System.out.println(input);
-			Statement st7 = cn.createStatement();
-			ResultSet rs7 = st7.executeQuery(input);
-			while (rs7.next())
-			{
-				System.out.println(rs7.getString(1));
-			}
-		}catch (SQLException e){
-			System.out.println("Query7 failes: " +e);
-		}
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			String s = in.readLine();
-			System.out.println(s);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		try {
-			cn.close();
-		}catch (SQLException e) 
-		{
-
-			e.printStackTrace();
 		}
 	}
 
@@ -132,24 +45,73 @@ public class User {
 		System.out.println("hello world!");
 	}
 
-	public void adminExecuteOperation(int option) {
-		System.out.println("We are going to execute this operation: " );
-		switch(option) {
-			case 1:
-				System.out.println("ok here's case 1");
-			case 2:
-				System.out.println("here case 2");
-			case 3:
-				System.out.println("here is case 3");
-			case 4:
-				System.out.println("here is case 4");
 
+	// Method to execute administration operation
+	public void adminExecuteOperation(int option, String dbname, String userID, String password) {
+		System.out.println("We are going to execute this operation: " );
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+dbname, userID, password);
+
+			switch(option) {
+				case 1: //update
+					System.out.println("ok here's case 1. You're need to type in info for instance we want to add!");
+					BufferedReader instanceInformation = new BufferedReader(new InputStreamReader(System.in));
+					// String newInstance = instanceInformation.readLine();
+					// System.out.println(newInstance);
+					try {
+						System.out.println("show databases");
+						Statement st1 = cn.createStatement();
+						ResultSet rs1 = st1.executeQuery("show databases");
+						while (rs1.next())
+							{
+								System.out.println("Database: "+rs1.getString(1));
+							}
+							st1.close();
+					} 
+					catch (SQLException e) {
+						System.out.println("Query failed: " + e);
+					}
+					break;
+				case 2: //delete
+					System.out.println("here case 2");
+					break;
+			}
+
+		}
+		catch (Exception e) {
+			System.out.println("connection failed: " + e);
 		}
 	}
 
-	// public void mainMenu() {
-	// 	System.out.println("MAIN MENU");
-	// }
+
+	/*******BHAVIN YOUR CODE GOES HERE!!***********/
+	public void userExecuteOperation(int option) {
+		System.out.println("We are going to execute this user query: " + option);
+
+		switch(option) {
+			case 1:
+				//query 1
+				break;
+			case 2:
+				//query 2
+				break;
+			case 3:
+				//query 3
+				break;
+			case 4:
+				//query 4
+				break;
+			case 5:
+				//query 5
+				break;
+			case 6:
+				//query 6
+				break;
+		}
+
+	}
 
 	public static void main(String[] args){
 		String dbname = "bgala";
@@ -179,25 +141,27 @@ public class User {
 				System.out.println("You're in admin mode. You can now do the following: ");
 				System.out.println(adminOptionsText);
 				BufferedReader inAdminChoice = new BufferedReader(new InputStreamReader(System.in));
-				int adminChoice = Integer.parseInt(inAdminChoice.readLine());
+				int adminChoice = Integer.parseInt(inAdminChoice.readLine()); //admin update/delete choice
 				
 				System.out.println("Great choice! " + adminChoice);
 
-				User app = new User(dbname, userID, password);
-				app.helloWorld();
-				app.adminExecuteOperation(adminChoice);
-				//okay now execute that command 
-				//okay, now execute the command on the choice that is passed through
+				User userTmp = new User(dbname, userID, password);
+
+				userTmp.adminExecuteOperation(adminChoice, dbname, userID, password);
+
 			}
 
 			else if (typeUser == 2 ) {
 				System.out.println("hey we are in user status");
 				System.out.println(userOptionsText);
 				BufferedReader inUserChoice = new BufferedReader(new InputStreamReader(System.in));
-				int userChoice = Integer.parseInt(inUserChoice.readLine());
+				
+				int userChoice = Integer.parseInt(inUserChoice.readLine()); //user's query choice
 
 				System.out.println("Great choice! " + userChoice);
-				//okay now execute that command
+
+				User userTmp = new User(dbname, userID, password);
+				userTmp.userExecuteOperation(userChoice);
 			}
 
 			else {
